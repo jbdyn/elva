14d464cd6832d49a93e085022dd7850321f895c6
Jakob Zahn <jakob.zahn@mailbox.org>
2024-08-20 16:48:08 +0200

    Test request interruption during Websocket handshake for Basic Auth

--- experiments/simple-server.py
+++ experiments/simple-server.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python
 
 import asyncio
+import base64
 
 from websockets.server import serve
 
 
+async def process_request(path, request_headers):
+    print("processing request headers")
+    print(request_headers)
+    auth_method, credentials = request_headers["Authorization"].split(" ", maxsplit=1)
+    print("CREDENTIALS", credentials)
+    user, password = (
+        base64.b64decode(credentials.encode()).decode().split(":", maxsplit=1)
+    )
+    print(user, password)
+
+
 async def print_message(websocket):
+    print(websocket, " connected!")
     async for message in websocket:
         print(message)
 
 
 async def main():
-    async with serve(print_message, "localhost", 8000):
+    async with serve(print_message, "localhost", 8000, process_request=process_request):
+        print("serving...")
         await asyncio.Future()  # run forever
 
 
--- experiments/test-websockets.py
+++ experiments/test-websockets.py
@@ -1,17 +1,29 @@
-import websockets
+import base64
+
 import anyio
+import websockets
+
 
 async def loop(ws):
     while True:
         await ws.send("test")
         await anyio.sleep(0.5)
 
+
 async def main():
-    async for ws in websockets.connect("ws://localhost:8000"):
+    user = "johndoe"
+    password = "janedoe"
+    value = "{}:{}".format(user, password).encode()
+    b64value = base64.b64encode(value).decode()
+    assert type(b64value) is str
+    headers = dict(Authorization="Basic " + b64value)
+    print(headers)
+    async for ws in websockets.connect("ws://johndoe:janedoe@localhost:8000"):
         try:
-            await anyio.sleep_forever()
+            await loop(ws)
         except Exception as e:
             print(e)
             print(ws)
+
 
 anyio.run(main)
