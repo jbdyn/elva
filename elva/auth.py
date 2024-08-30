f824dbe0c79def283841dcdf4cc89311b94aae2d
Jakob Zahn <jakob.zahn@mailbox.org>
2024-08-30 23:04:09 +0200

    WIP: Add Basic Auth class for use in websockets.serve, add examples, provide LDAP self-bind function

--- elva/auth.py
+++ elva/auth.py
@@ -1,19 +1,84 @@
+from base64 import b64decode
 from getpass import getpass
+from http import HTTPStatus
 
 import ldap3
 from ldap3.core.exceptions import LDAPException
 
-LDAP_SERVER = "example-ldap.com"
-LDAP_BASE = "ou=user,dc=example,dc=com"
+AUTH_SCHEME = [
+    "Basic",
+    "Digest",
+    "Negotiate",
+]
+
+
+def process_authorization_header(request_headers):
+    auth_header = request_headers["Authorization"]
+    scheme, credentials = auth_header.split(" ", maxsplit=1)
+    if scheme not in AUTH_SCHEME:
+        raise ValueError("invalid scheme in Authorization header")
+    return scheme, credentials
+
+
+def process_basic_auth_credentials(credentials: str):
+    bb64cred = credentials.encode()
+    bcred = b64decode(bb64cred)
+    cred = bcred.decode()
+    username, password = cred.split(":", maxsplit=1)
+    return username, password
+
+
+def abort_basic_auth(
+    realm: str,
+    reason: None | str = None,
+    status: None | HTTPStatus = None,
+):
+    status = status or HTTPStatus.UNAUTHORIZED
+    headers = {"WWW-Authenticate": f"Basic realm={realm}"}
+    if reason:
+        reason = reason.encode()
+    return status, headers, reason
+
+
+class BasicAuth:
+    scheme = "Basic"
+
+    def __init__(self, realm):
+        self.realm = realm
+
+    def authenticate(self, path, request_headers):
+        try:
+            scheme, credentials = process_authorization_header(request_headers)
+        except KeyError:
+            return self.abort("missing Authorization header")
+        except ValueError:
+            return self.abort("malformed Authorization header")
+
+        match scheme:
+            case "Basic":
+                username, password = process_basic_auth_credentials(credentials)
+            case _:
+                return self.abort("unsupported Authorization scheme")
+
+        if not self.verify(username, password):
+            return self.abort("invalid credentials")
+
+    def abort(self, reason=None, status=None):
+        return abort_basic_auth(self.realm, reason=reason, status=status)
+
+    def verify(self, username, password): ...
 
 
 # code from https://stackoverflow.com/a/65907735
-def ldap_login(server, username, password):
+def ldap_self_bind(username, password, server, ldap_base):
     try:
+        user = f"uid={username},{ldap_base}"
         with ldap3.Connection(
-            server, user=f"uid={username},{LDAP_BASE}", password=password
-        ) as conn:
-            # print(conn.result["description"])  # "success" if bind is ok
+            server,
+            user=user,
+            password=password,
+        ):  # as conn
+            # res = conn.result["description"]  # "success" if bind is ok
             return True
     except LDAPException:
         # print("Unable to connect to LDAP server")
@@ -21,5 +86,9 @@ def ldap_login(server, username, password):
 
 
 if __name__ == "__main__":
-    server = ldap3.Server(LDAP_SERVER, use_ssl=True)
-    ldap_login(server, input("Username: "), getpass())
+    ldap_server = "example-ldap.com"
+    ldap_base = "ou=user,dc=example,dc=com"
+
+    server = ldap3.Server(ldap_server, use_ssl=True)
+
+    ldap_self_bind(input("Username: "), getpass(), server, ldap_base)
--- experiments/simple-server.py
+++ experiments/simple-server.py
@@ -1,35 +1,29 @@
 #!/usr/bin/env python
 
 import asyncio
-import base64
-import http
 
 import ldap3
 from websockets.server import serve
 
-from elva.auth import LDAP_SERVER
+from elva.auth import BasicAuth, ldap_self_bind
 
-SERVER = ldap3.Server(LDAP_SERVER, use_ssl=True)
+LDAP_SERVER = "example-ldap.com"
+LDAP_BASE = "ou=user,dc=example,dc=com"
 
 
-async def process_request(path, request_headers):
-    print("processing request headers")
-    print(request_headers)
-    auth_method, credentials = request_headers["Authorization"].split(" ", maxsplit=1)
-    print("CREDENTIALS", credentials)
-    user, password = (
-        base64.b64decode(credentials.encode()).decode().split(":", maxsplit=1)
-    )
-    # success = ldap_login(SERVER, user, password)
-    success = user == "johndoe" and password == "janedoe"
-    del password
-    if not success:
-        # abort handshake
-        # returns (response status, headers, body)
-        return http.HTTPStatus.OK, [], b"wrong credentials"
-    else:
-        # continue handshake normally
-        return None
+class DummyBasicAuth(BasicAuth):
+    def verify(self, username, password):
+        return username == "johndoe" and password == "janedoe"
+
+
+class LDAPBasicAuth(BasicAuth):
+    def __init__(self, realm, server, base):
+        super().__init__(realm)
+        self.server = ldap3.Server(server, use_ssl=True)
+        self.base = base
+
+    def verify(self, username, password):
+        return ldap_self_bind(username, password, self.server, self.base)
 
 
 async def print_message(websocket):
@@ -39,7 +33,13 @@ async def print_message(websocket):
 
 
 async def main():
-    async with serve(print_message, "localhost", 8000, process_request=process_request):
+    async with serve(
+        print_message,
+        "localhost",
+        8000,
+        process_request=DummyBasicAuth("dummy").authenticate,
+        # process_request=TUBLDAPBasicAuth("tub", LDAP_SERVER, LDAP_BASE),
+    ):
         print("serving...")
         await asyncio.Future()  # run forever
 
