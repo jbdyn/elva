82ac3f246a8697b86a31ba9a0f96242909337a31
Jakob Zahn <jakob.zahn@mailbox.org>
2024-11-15 12:38:43 +0100

    Adapt config parameter names, define StatusBar widgets, define actions

--- src/elva/apps/editor.py
+++ src/elva/apps/editor.py
@@ -1,5 +1,4 @@
 import logging
-from copy import deepcopy
 from pathlib import Path
 
 import click
@@ -11,7 +10,6 @@ from textual.widgets import Button
 from textual.worker import WorkerState
 from websockets.exceptions import InvalidStatus
 
-from elva.component import Component
 from elva.log import LOGGER_NAME, DefaultFormatter
 from elva.provider import ElvaWebsocketProvider, WebsocketProvider
 from elva.renderer import TextRenderer
@@ -23,12 +21,12 @@ from elva.widgets.config import (
     PathSuggester,
     QRCodeView,
     RadioSelectView,
-    StatusBar,
     SwitchView,
     TextInputView,
     URLInputView,
     WebsocketsURLValidator,
 )
+from elva.widgets.status import ComponentStatus, FeatureStatus, StatusBar
 from elva.widgets.textarea import YTextArea
 
 log = logging.getLogger(__name__)
@@ -70,39 +68,17 @@ def encode_content(data):
     return tomli_w.dumps(data)
 
 
-class FeatureStatus(Button):
-    def __init__(self, params, *args, config=None, param_map=None, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.params = params
-        self.config = self.trim(config)
-        self.param_map = param_map
-
-    def trim(self, config):
-        return dict((param, config.get(param)) for param in self.params)
-
-    def update(self, config):
-        trimmed = self.trim(config)
-        changed = trimmed != self.config
-
-        if self.param_map is not None:
-            for from_param, to_param in self.param_map.items():
-                trimmed[to_param] = trimmed.pop(from_param)
-
-        self.config = trimmed
-        if changed:
-            self.apply()
-
-    @property
-    def is_ready(self): ...
-
-    def apply(self): ...
-
-
 class LogStatus(FeatureStatus):
     @property
     def is_ready(self):
-        path = self.config.get("path")
-        return path is not None and len(path.suffixes) > 0 and not path.is_dir()
+        c = self.config
+        path = c.get("path")
+        return (
+            path is not None
+            and len(path.suffixes) > 0
+            and not path.is_dir()
+            and c.get("level") is not None
+        )
 
     def apply(self):
         if self.is_ready:
@@ -118,36 +94,15 @@ class LogStatus(FeatureStatus):
             log.setLevel(c.get("level") or logging.INFO)
             self.variant = "success"
         else:
+            for handler in log.handlers[:]:
+                log.removeHandler(handler)
             self.variant = "default"
-
-
-class ComponentStatus(FeatureStatus):
-    component: Component
-
-    def __init__(self, yobject, params, *args, **kwargs):
-        self.yobject = yobject
-        self.instance = None
-        super().__init__(params, *args, **kwargs)
-
-    def apply(self):
-        if self.is_ready:
-            component = self.component(self.yobject, **self.config)
-            self.run_worker(component.start(), exclusive=True, exit_on_error=False)
-            self.variant = "success"
-        else:
-            self.workers.cancel_node(self)
-            self.variant = "default"
-
-    def on_worker_state_changed(self, message):
-        match message.state:
-            case WorkerState.ERROR:
-                self.variant = "error"
-            case WorkerState.CANCELLED | WorkerState.SUCCESS:
-                self.variant = "default"
 
     def on_button_pressed(self, message):
-        if message.button.variant == "success":
-            self.workers.cancel_node(self)
+        if self.variant == "success":
+            for handler in log.handlers[:]:
+                log.removeHandler(handler)
+            self.variant = "default"
         else:
             self.apply()
 
@@ -166,6 +121,18 @@ class StoreStatus(ComponentStatus):
             and c.get("identifier") is not None
         )
 
+    def on_worker_state_changed(self, message):
+        if message.worker.name == "component":
+            match message.state:
+                case WorkerState.RUNNING:
+                    self.variant = "success"
+                case WorkerState.ERROR:
+                    self.variant = "error"
+                    self.control = None
+                case WorkerState.CANCELLED | WorkerState.SUCCESS:
+                    self.variant = "default"
+                    self.control = None
+
 
 class RendererStatus(ComponentStatus):
     component = TextRenderer
@@ -175,6 +142,18 @@ class RendererStatus(ComponentStatus):
         path = self.config.get("path")
         return path is not None and len(path.suffixes) > 0 and not path.is_dir()
 
+    def on_worker_state_changed(self, message):
+        if message.worker.name == "component":
+            match message.state:
+                case WorkerState.RUNNING:
+                    self.variant = "success"
+                case WorkerState.ERROR:
+                    self.variant = "error"
+                    self.control = None
+                case WorkerState.CANCELLED | WorkerState.SUCCESS:
+                    self.variant = "default"
+                    self.control = None
+
 
 class ProviderStatus(ComponentStatus):
     component = get_provider
@@ -183,6 +162,27 @@ class ProviderStatus(ComponentStatus):
     def is_ready(self):
         c = self.config
         return c.get("identifier") and c.get("server")
+
+    def on_worker_state_changed(self, message):
+        if message.worker.name == "component":
+            match message.state:
+                case WorkerState.RUNNING:
+                    self.variant = "warning"
+                    self.run_worker(self.watch_connection_status(), group="event")
+                case WorkerState.ERROR:
+                    self.variant = "error"
+                    self.control = None
+                case WorkerState.CANCELLED | WorkerState.SUCCESS:
+                    self.variant = "default"
+                    self.control = None
+
+    async def watch_connection_status(self):
+        while True:
+            await self.control.connected.wait()
+            self.variant = "success"
+
+            await self.control.disconnected.wait()
+            self.variant = "warning"
 
 
 class UI(App):
@@ -232,17 +232,8 @@ class UI(App):
 
     async def on_mount(self):
         self.query_one(ConfigPanel).add_class("hidden")
-        # self.run_worker(self.run_components())
-        # check existence of files before anything is changed on disk
-        # if self.render_path is not None:
-        #    try:
-        #        no_file = not self.file_path.exists()
-        #    except Exception:
-        #        no_file = True
-        #    if self.render_path.exists() and no_file:
-        #        async with await anyio.open_file(self.render_path, "r") as file:
-        #            text = await file.read()
-        #            self.ytext += text
+        self.query_one(StoreStatus).disabled = True
+        self.query_one(RendererStatus).disabled = True
 
     def compose(self):
         c = self.config
@@ -296,12 +287,12 @@ class UI(App):
                     id="view-password",
                 ),
                 PathInputView(
-                    value=c.get("file_path"),
+                    value=c.get("file"),
                     suggester=PathSuggester(),
                     name="file_path",
                 ),
                 PathInputView(
-                    value=c.get("render_path"),
+                    value=c.get("render"),
                     suggester=PathSuggester(),
                     name="render_path",
                 ),
@@ -311,9 +302,9 @@ class UI(App):
                     animate=False,
                 ),
                 PathInputView(
-                    value=c.get("log_path"),
+                    value=c.get("log"),
                     suggester=PathSuggester(),
-                    name="log_path",
+                    name="log",
                 ),
                 RadioSelectView(
                     list(LOG_LEVEL_MAP.items()),
@@ -343,32 +334,32 @@ class UI(App):
                 self.ydoc,
                 [
                     "identifier",
-                    "file_path",
+                    "file",
                 ],
                 "S",
                 config=self.config,
-                param_map={"file_path": "path"},
+                rename={"file": "path"},
                 id="store",
             )
             yield RendererStatus(
                 self.ytext,
                 [
-                    "render_path",
+                    "render",
                     "auto_render",
                 ],
                 "R",
                 config=self.config,
-                param_map={"render_path": "path"},
+                rename={"render": "path"},
                 id="renderer",
             )
             yield LogStatus(
                 [
-                    # level is always set, regardless whether it has changed or nog
-                    "log_path",
+                    "log",
+                    "level",
                 ],
                 "L",
                 config=self.config,
-                param_map={"log_path": "path"},
+                rename={"log": "path"},
                 id="logger",
             )
 
@@ -380,7 +371,7 @@ class UI(App):
 
     def on_config_view_saved(self, message):
         c = self.query_one(ConfigPanel).state
-        file_path = c.get("file_path")
+        file_path = c.get("file")
         if file_path is not None and file_path.suffix and not file_path.is_dir():
             SQLiteStore.set_metadata(file_path, {message.name: message.value})
 
@@ -399,19 +390,21 @@ class UI(App):
         self.query_one("#view-share").value = content
 
     def action_render(self):
-        if self.render_path is not None:
-            self.run_worker(self.renderer.write())
+        renderer = self.query_one("#renderer").control
+        if renderer is not None:
+            self.run_worker(renderer.write())
+        else:
+            self.query_one(ConfigPanel).remove_class("hidden")
 
-            # the user explicitly wants this to be rendered,
-            # so also enable the auto-rendering on closing
-            self.renderer.render = True
-
-    def action_save(self): ...
+    def action_save(self):
+        store = self.query_one("#store").control
+        if store is None:
+            self.query_one(ConfigPanel).remove_class("hidden")
 
     @property
     def language(self):
         c = self.config
-        file_path = c.get("file_path")
+        file_path = c.get("file")
         if file_path is not None and file_path.suffix:
             suffix = "".join(file_path.suffixes).split(FILE_SUFFIX)[0].removeprefix(".")
             if str(file_path).endswith(suffix):
@@ -476,8 +469,8 @@ def cli(
 
     # logging
     LOGGER_NAME.set(__name__)
-    level = c["level"]
-    log_path = c["log"]
+    log_path = c.get("log")
+    level = c.get("level")
     if level is not None and log_path is not None:
         handler = logging.FileHandler(log_path)
         handler.setFormatter(DefaultFormatter())
