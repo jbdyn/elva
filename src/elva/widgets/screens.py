8c4d36fee7caf7cadf887824ac98061085b190ad
Jakob Zahn <jakob.zahn@mailbox.org>
2025-04-11 18:46:51 +0200

    WIP: Add API reference for status and screens widget modules

--- src/elva/widgets/screens.py
+++ src/elva/widgets/screens.py
@@ -1,5 +1,12 @@
+"""
+Screens for ELVA apps.
+"""
+
+from typing import Any
+
 from rich.text import Text as RichText
 from textual.containers import Grid
+from textual.message import Message
 from textual.screen import ModalScreen
 from textual.widgets import Button, Input, Static
 
@@ -7,7 +14,31 @@ from elva.auth import basic_authorization_header
 
 
 class CredentialScreen(ModalScreen):
-    def __init__(self, options, body=None, user=None):
+    """
+    Modal screen providing a form for credential input.
+    """
+
+    options: dict
+    """Mapping of options for the connection provider."""
+
+    body: Static
+    """Instance of the static widget displaying an informational message."""
+
+    user: Input
+    """Instance of the input widget for the user name."""
+
+    password: Input
+    """Instance of the input widget for the password."""
+
+    def __init__(
+        self, options: dict[str, Any], body: None | str = None, user: None | str = None
+    ):
+        """
+        Arguments:
+            options: mapping of options for the connection provider.
+            body: informational message displayed above the credential form.
+            user: user name
+        """
         super().__init__(classes="modalscreen", id="credentialscreen")
         self.options = options
         self.body = Static(RichText(body, justify="center"), id="body")
@@ -17,6 +48,9 @@ class CredentialScreen(ModalScreen):
         self.password = Input(placeholder="password", password=True, id="password")
 
     def compose(self):
+        """
+        Hook arranging child widgets.
+        """
         with Grid(classes="form"):
             yield self.body
             yield self.user
@@ -24,25 +58,57 @@ class CredentialScreen(ModalScreen):
             yield Button("Confirm", classes="confirm")
 
     def update_and_return_credentials(self):
+        """
+        Save input credentials and return them after closing the screen.
+
+        This method saves the credentials encoded in a basic authorization header in the `options` attribute for usage in the connection provider.
+        """
         credentials = (self.user.value, self.password.value)
         header = basic_authorization_header(*credentials)
         self.options["additional_headers"] = header
         self.password.clear()
         self.dismiss(credentials)
 
-    def on_button_pressed(self, event):
+    def on_button_pressed(self, event: Message):
+        """
+        Hook called on a button pressed event.
+
+        The credentials get updated and the screen closed.
+
+        Arguments:
+            event: message object holding information about the button pressed event.
+        """
         self.update_and_return_credentials()
 
     def key_enter(self):
+        """
+        Hook called on an enter pressed event.
+
+        The credentials get updated and the screen closed.
+        """
         self.update_and_return_credentials()
 
 
 class ErrorScreen(ModalScreen):
-    def __init__(self, exc):
+    """
+    Modal screen displaying an exception message.
+    """
+
+    exc: str
+    """The exception message to display."""
+
+    def __init__(self, exc: str):
+        """
+        Arguments:
+            exc: the exception message to display.
+        """
         super().__init__(classes="modalscreen", id="errorscreen")
         self.exc = exc
 
     def compose(self):
+        """
+        Hook arranging child widgets.
+        """
         with Grid(classes="form"):
             yield Static(
                 RichText(
@@ -53,5 +119,13 @@ class ErrorScreen(ModalScreen):
             yield Static(RichText(str(self.exc), justify="center"))
             yield Button("OK", classes="confirm")
 
-    def on_button_pressed(self, event):
+    def on_button_pressed(self, event: Message):
+        """
+        Hook called on a button pressed event.
+
+        It closes the screen.
+
+        Arguments:
+            event: the message object holding information about the button pressed event.
+        """
         self.dismiss()
