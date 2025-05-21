371eaf9a8f4b1c746e1e13d521dea2463d449b50
Jakob Zahn <jakob.zahn@mailbox.org>
2025-05-21 20:01:42 +0200

    Add tests for the `protocol` module

--- src/elva/protocol.py
+++ src/elva/protocol.py
@@ -2,51 +2,119 @@
 Module holding the [Y-Protocol](https://github.com/yjs/y-protocols/blob/master/PROTOCOL.md) specification.
 """
 
-from codecs import Codec, IncrementalDecoder, IncrementalEncoder
+from codecs import Codec
 from enum import Enum
 from typing import Self
 
+##
+#
+# HELPERS
+#
 
-def write_var_uint(data: bytes) -> tuple[bytes, int]:
+
+def write_var_uint(num: int) -> list[int]:
     """
-    Calculate the variable unsigned integer length of `data`.
+    Encode an integer into a variable unsigned integer.
 
     Arguments:
-        data: bytes object.
+        num: the integer to encode.
+
+    Returns:
+        a list of integers representing the bits of the variable unsigned integer.
+    """
+    res = []
+
+    # while `num` is bigger than 1 byte minus 1 continuation bit, i.e. 7 bits
+    while num > 127:
+        #        127 & num   <=>  extract the last 7 bits of `num`
+        # 128 | (127 & num)  <=>  put a "1" as continuation bit before the last 7 bits of `num`
+        res.append(128 | (127 & num))
+
+        # discard the last 7 bits of `num`
+        num >>= 7
+
+    # append the remaining bits in `num`
+    res.append(num)
+
+    return res
+
+
+def read_var_uint(data: bytes) -> tuple[int, int]:
+    """
+    Read the first variable unsigned integer in `data`.
+
+    Arguments:
+        data: data holding the variable unsigned integer.
+
+    Returns:
+        a tuple of the decoded variable unsigned integer and its number of bytes
+    """
+    # we start at the very first byte/bit of `data`
+    uint = 0
+    bit = 0
+    byte_idx = 0
+
+    while True:
+        byte = data[byte_idx]
+
+        #  byte & 127          <=>  extract the last 7 bits in the current `byte`
+        # (byte & 127) << bit  <=>  shift bits in `res` `bit` times to the left
+        uint += (byte & 127) << bit
+
+        # move the bit offset by 7
+        bit += 7
+
+        # move the byte offset by 1
+        byte_idx += 1
+
+        if byte < 128:
+            # the first bit of this byte, i.e. the continuation bit, is not a "1",
+            # so this is the last byte being part of the variable unsigned integer
+            break
+
+    return uint, byte_idx
+
+
+def prepend_var_uint(data: bytes) -> tuple[bytes, int]:
+    """
+    Prepend the length of `data` as variable unsigned integer to `data`.
+
+    See [Yjs base encoding](https://github.com/yjs/y-protocols/blob/master/PROTOCOL.md#base-encoding-approaches) for reference.
+
+    Arguments:
+        data: the payload of a Y protocol message.
 
     Returns:
         A tuple of two values: `data` with the variable unsigned integer prepended and the length of `data`.
     """
-    num = len(data)
-    res = []
-    while num > 127:
-        res.append(128 | (127 & num))
-        num >>= 7
-    res.append(num)
-    return bytes(res) + data, len(data)
+    len_data = len(data)
+    res = write_var_uint(len_data)
+
+    return bytes(res) + data, len_data
 
 
-def read_var_uint(data: bytes) -> tuple[bytes, int]:
+def strip_var_uint(data: bytes) -> tuple[bytes, int]:
     """
-    Read and strip off the variable unsigned interger value from `data`.
+    Read and strip off the variable unsigned integer length from `data`.
+
+    See [Yjs base encoding](https://github.com/yjs/y-protocols/blob/master/PROTOCOL.md#base-encoding-approaches) for reference.
 
     Arguments:
-        data: bytes object.
+        data: the payload of a Y protocol message plus its length as variable unsigned integer prepended.
 
     Returns:
-        A tuple of two values: `data` with the variable unsigned integer stripped and the length of bytes of `data` being processed.
+        A tuple of two values: `data` with the variable unsigned integer length stripped and the length of bytes of `data` being processed.
     """
-    uint = 0
-    bit = 0
-    byte_idx = 0
-    while True:
-        byte = data[byte_idx]
-        uint += (byte & 127) << bit
-        bit += 7
-        byte_idx += 1
-        if byte < 128:
-            break
+    uint, byte_idx = read_var_uint(data)
+
+    # extract the payload from `data`, return the actual length of the payload
     return data[byte_idx : byte_idx + uint], min(byte_idx + uint, len(data))
+
+
+##
+#
+# CODEC
+#
 
 
 class YCodec(Codec):
@@ -59,62 +127,26 @@ class YCodec(Codec):
         Prepend the size of `payload` to itself as a variable unsigned integer.
 
         Arguments:
-            payload: data to encode
+            payload: the payload of a Y protocol message.
             errors: no-op.
 
         Returns:
             A tuple of two values: `data` with the variable unsigned integer prepended and the length of `data`.
         """
-        return write_var_uint(payload)
+        return prepend_var_uint(payload)
 
     def decode(self, message: bytes, errors: str = "strict") -> tuple[bytes, int]:
         """
-        Read and strip off the encoded size from `payload`.
+        Read and strip off the encoded size from `message`.
 
         Arguments:
-            message: data to decode
+            message: the payload of a Y protocol message plus its length as variable unsigned integer prepended.
             errors: no-op.
 
         Returns:
             A tuple of two values: `data` with the variable unsigned integer stripped and the length of bytes of `data` being processed.
         """
-        return read_var_uint(message)
-
-
-class YIncrementalEncoder(IncrementalEncoder):
-    state = 0
-
-    def encode(self, payloads):
-        message, length = write_var_uint(payloads[self.state])
-        self.state += 1
-        return message, length
-
-    def reset(self):
-        self.state = 0
-
-    def getstate(self):
-        return self.state
-
-    def setstate(self, state):
-        self.state = state
-
-
-class YIncrementalDecoder(IncrementalDecoder):
-    state = 0
-
-    def decode(self, message):
-        payload, length = read_var_uint(message[self.state :])
-        self.state += length
-        return payload, length
-
-    def reset(self):
-        self.state = 0
-
-    def getstate(self):
-        return self.state
-
-    def setstate(self, state):
-        self.state = state
+        return strip_var_uint(message)
 
 
 class Message(YCodec, Enum):
@@ -132,12 +164,23 @@ class Message(YCodec, Enum):
     def __repr__(self):
         return f"{self.__class__.__name__}.{self.name}"
 
+    @classmethod
+    @property
+    def types(cls) -> tuple[str]:
+        """
+        The message types associated with this codec.
+
+        Returns:
+            a tuple containing all message type names as strings.
+        """
+        return tuple(attr for attr in dir(cls) if not attr.startswith("__"))
+
     def encode(self, payload: bytes, errors: str = "strict") -> tuple[bytes, int]:
         """
         Calculate the encoded `payload` with the message type's magic bytes prepended.
 
         Arguments:
-            payload: data to be encoded.
+            payload: the payload of a Y protocol message.
             errors: no-op.
 
         Returns:
@@ -148,18 +191,31 @@ class Message(YCodec, Enum):
 
     def decode(self, message: bytes, errors: str = "strict") -> tuple[bytes, int]:
         """
-        Remove magic bytes and decode `message`.
+        Remove magic bytes and extract the payload of `message`.
 
         Arguments:
-            message: data to be decoded.
+            message: the Y protocol message.
             errors: no-op.
 
         Returns:
             A tuple of two objects: the decoded message with the message type's magic bytes removed and the length of bytes being processed.
         """
         message = message.removeprefix(self.magic_bytes)
-        payload, length = super().decode(message, errors=errors)
+        payload, length = self._decode(message, errors=errors)
         return payload, length + len(self.magic_bytes)
+
+    def _decode(self, data: bytes, errors: str = "strict") -> tuple[bytes, int]:
+        """
+        Hook extracting the payload of `message`.
+
+        Arguments:
+            data: the payload of a Y protocol message plus its length as variable unsigned integer prepended.
+            errors: no-op.
+
+        Returns:
+            A tuple of two objects: `data` with the variable unsigned integer stripped and the length of bytes of `data` being processed.
+        """
+        return super().decode(data, errors=errors)
 
     @classmethod
     def infer_and_decode(
@@ -169,34 +225,52 @@ class Message(YCodec, Enum):
         Infer the type of the given message and return its decoded form.
 
         Arguments:
-            message: data to decode
+            message: the Y protocol message.
             errors: no-op.
 
         Returns:
-            A tuple of three objects: the inferred message type of `message`, the decoded form of `message` and the length of processed bytes from `message`
+            A tuple of three objects: the inferred message type of `message`, the decoded form of `message` and the length of processed bytes from `message`.
         """
-        first = message[0]
-        match first:
-            case 0:
-                ymsg = cls((first, message[1]))
-                return ymsg, *ymsg.decode(message, errors=errors)
-            case 1:
-                ymsg = cls((first,))
-                return ymsg, *ymsg.decode(message, errors=errors)
-            case _:
-                raise ValueError(f"given message '{message}' is not a valid YMessage")
+        # there might be no second magic byte
+        mb2_off = 0
+
+        mb1, mb1_off = read_var_uint(message)
+
+        try:
+            if mb1 == 1:
+                # awareness message is the only type with a single magic byte
+                ymsg = cls((mb1,))
+            else:
+                mb2, mb2_off = read_var_uint(message[mb1_off:])
+                ymsg = cls((mb1, mb2))
+        except ValueError:
+            raise ValueError(
+                f"Message with magic bytes {mb1}, {mb2} is not a valid {cls.__name__}"
+            ) from None
+
+        payload, length = ymsg._decode(message[mb1_off + mb2_off :], errors=errors)
+        return ymsg, payload, mb1_off + mb2_off + length
+
+
+##
+#
+# PROTOCOLS
+#
+
+# be aware to give integers from `*write_var_uint`;
+# here we stay below 127 and thus `write_var_uint` is not needed.
 
 
 class YMessage(Message):
     """
-    Implementation of Y messages according to the [Yjs sync and awareness protocol](https://github.com/yjs/y-protocols/blob/master/PROTOCOL.md#sync-protocol-v1-encoding)
+    Implementation of Y messages according to the [Yjs sync and awareness protocol](https://github.com/yjs/y-protocols/blob/master/PROTOCOL.md#sync-protocol-v1-encoding).
     """
 
     SYNC_STEP1 = (0, 0)
     """Synchronization request message."""
 
     SYNC_STEP2 = (0, 1)
-    """synchronization reply message."""
+    """Synchronization reply message."""
 
     SYNC_UPDATE = (0, 2)
     """Update message."""
@@ -205,7 +279,6 @@ class YMessage(Message):
     """Awareness message."""
 
 
-# TODO: rewrite with letter magic bytes
 class ElvaMessage(Message):
     """
     Extension of Y messages with additional message types.
@@ -230,23 +303,23 @@ class ElvaMessage(Message):
     AWARENESS = (1,)
     """Awareness message."""
 
-    ID = (2, 0)
+    ID = (ord("I"), ord("D"))
     """Identitity message."""
 
-    READ = (2, 1)
+    READ = (ord("R"), ord("O"))
     """Read-only message."""
 
-    READ_WRITE = (2, 2)
+    READ_WRITE = (ord("R"), ord("W"))
     """Read-write message."""
 
-    DATA_REQUEST = (3, 0)
+    DATA_REQUEST = (ord("D"), 1)
     """Message requesting a specific blob of data."""
 
-    DATA_OFFER = (3, 1)
+    DATA_OFFER = (ord("D"), 2)
     """Message offering a requested blob of data."""
 
-    DATA_ORDER = (3, 2)
-    """Message ordering a offered blob of data."""
+    DATA_ORDER = (ord("D"), 3)
+    """Message ordering an offered blob of data."""
 
-    DATA_TRANSFER = (3, 3)
-    """Message transferring a ordered blob of data."""
+    DATA_TRANSFER = (ord("D"), 4)
+    """Message transferring an ordered blob of data."""
