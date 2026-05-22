32288d84a5acb87546ed4260b831c9ecfb67e394
Jakob Zahn <jakob.zahn@mailbox.org>
2026-05-22 16:46:26 +0200

    Replace `setuptools.find_namespace_packages` with own implementation
    This avoids the long import time from `setuptools`

--- src/elva/main.py
+++ src/elva/main.py
@@ -7,18 +7,35 @@ Subcommands are defined in the respective app package.
 from importlib import import_module as import_
 from pathlib import Path
 
-from setuptools import find_namespace_packages
-
 from elva.cli import elva
 from elva.core import ELVA_COMMAND_DIR_NAME, get_command_import_path
 
+
+def commands() -> list[str]:
+    """
+    Get all namespace package names from the ELVA command directory.
+
+    Returns:
+        the namespace package names in the ELVA command directory.
+    """
+    root = Path(__file__).parent / ELVA_COMMAND_DIR_NAME
+
+    commands = list()
+
+    # check for namespace package
+    for file in root.iterdir():
+        if file.is_dir() and (file / "__init__.py").exists():
+            commands.append(file.name)
+
+    return commands
+
+
 # read all other present commands
-pkgs = find_namespace_packages(Path(__file__).parent / ELVA_COMMAND_DIR_NAME)
-commands = [get_command_import_path(pkg) for pkg in pkgs]
+paths = [get_command_import_path(path) for path in commands()]
 
 # import `cli` functions of command packages
-for command in commands:
-    module = import_(command)
+for path in paths:
+    module = import_(path)
     elva.add_command(module.cli)
 
 if __name__ == "__main__":
