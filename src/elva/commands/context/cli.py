b36bbcd466274e6a00ca9893179f1b6cb1df6833
Jakob Zahn <jakob.zahn@mailbox.org>
2026-05-27 17:25:45 +0200

    Make `context` command dump config if specified

--- src/elva/commands/context/cli.py
+++ src/elva/commands/context/cli.py
@@ -3,6 +3,7 @@ from tomli_w import dumps
 
 from elva.cli import context, data, unset
 from elva.config import Config, convert
+from elva.files import Metadata
 
 TRANSLATIONS = {
     "config": "config",
@@ -27,10 +28,20 @@ def run(config: Config) -> None:
     # alias
     c = config
 
+    # extract interesting settings before
+    dump = c.get("config.dump", False)
+    replace = c.get("config.replace", True)
+
     if not c.get("context.config", False):
         c.pop("config", None)
 
-    config.pop("context", None)
+    # remove own context
+    own = config.pop("context", {})
+
+    # write to file if desired
+    if (file := own.get("data", None)) and dump:
+        with Metadata(file) as metadata:
+            metadata.set_config(config, replace=replace)
 
     echo(dumps(convert(config)))
 
